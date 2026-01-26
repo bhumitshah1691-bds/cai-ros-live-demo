384c14f3fe4092ac680d7b38afb071508127c8fa
Bhumit Shah <bhumit.shah1691@gmail.com>
2026-01-26 20:26:00 +0530

    Add DemoController component with centered demo text

---
+++ src/demo/DemoController.jsx
@@ -0,0 +1,15 @@
+function DemoController() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      width: '100vw'
+    }}>
+      CAI-ROS DEMO START
+    </div>
+  )
+}
+
+export default DemoController
